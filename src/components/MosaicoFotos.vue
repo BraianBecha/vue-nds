fd09f0625f4f43967b5ce9175a07bf48f7519f00
BraianBecha <braian.becha.hace@gmail.com>
2025-01-23 00:42:04 -0300

    cambio rutas de mosaico 13

--- src/components/MosaicoFotos.vue
+++ src/components/MosaicoFotos.vue
@@ -15,10 +15,10 @@ export default{
      
     <div class="w3-half">
      
-      <imagenConDescripcion pieDeFoto="Imagen del paracaídas del módulo que transportaba al rover Perseverance, momentos antes de aterrizar en el planeta rojo. Sol:1" :unLink = "'src/assets/fotos/EBF_0001_0667022756_679ECV_N0010052EDLC00001_0010LUJ01_1200.jpg'" />
-      <imagenConDescripcion pieDeFoto="Suelo marciano" :unLink = "'https://raw.githubusercontent.com/BraianBecha/vue-nds/refs/heads/main/src/assets/fotos/FRF_0688_0728025478_520ECM_N0321184FHAZ02418_01_295J01_1200.jpg'" />
-      <imagenConDescripcion pieDeFoto="Paisaje Marciano" :unLink = "'src/assets/fotos/NRF_0688_0728028560_620ECM_N0321184NCAM02688_09_095J01_1200.jpg'" />
-      <imagenConDescripcion pieDeFoto="El rover Perseverance capta la luna marciana Phobos eclipsando al Sol. Sol:1285" :unLink = '"../assets/fotos/ZL7_1056_0760664098_398EBY_N0501254ZCAM01538_1100LMJ01_1200.jpg"' />
+      <imagenConDescripcion :pieDeFoto="'Imagen del paracaídas del módulo que transportaba al rover Perseverance, momentos antes de aterrizar en el planeta rojo. Sol:1'" :unLink = "'src/assets/fotos/EBF_0001_0667022756_679ECV_N0010052EDLC00001_0010LUJ01_1200.jpg'" />
+      <imagenConDescripcion :pieDeFoto="'Suelo marciano'" :unLink = "'https://raw.githubusercontent.com/BraianBecha/vue-nds/refs/heads/main/src/assets/fotos/FRF_0688_0728025478_520ECM_N0321184FHAZ02418_01_295J01_1200.jpg'" />
+      <imagenConDescripcion :pieDeFoto="'Paisaje Marciano'" :unLink = "'src/assets/fotos/NRF_0688_0728028560_620ECM_N0321184NCAM02688_09_095J01_1200.jpg'" />
+      <imagenConDescripcion :pieDeFoto="'El rover Perseverance capta la luna marciana Phobos eclipsando al Sol. Sol:1285'" :unLink = '"../assets/fotos/ZL7_1056_0760664098_398EBY_N0501254ZCAM01538_1100LMJ01_1200.jpg"' />
       
     
        <!-- 
@@ -32,8 +32,7 @@ export default{
     </div>
 
     <div class="w3-half">
-      
-      <imagenConDescripcion pieDeFoto="Suelo marciano2" :unLink = "'./assets/fotos/ZL0_0789_0736981026_113EBY_N0390926ZCAM03648_1100LMJ01_1200.jpg'" />
+      <imagenConDescripcion pieDeFoto="Suelo marciano2" :unLink = "'src/assets/fotos/ZL0_0789_0736981026_113EBY_N0390926ZCAM03648_1100LMJ01_1200.jpg'" />
       <imagenConDescripcion pieDeFoto="Suelo marcianox" :unLink = "'../assets/fotos/ZL0_0789_0736981026_113EBY_N0390926ZCAM03648_1100LMJ01_1200.jpg'" /> <link rel="stylesheet" href="">
       <imagenConDescripcion pieDeFoto="Suelo marciano" :unLink = "'/vue-nds/src/assets/fotos/ZR0_0210_0685583506_348EBY_N0071836ZCAM03233_1100LMJ02_1200perseverance.jpg'" />
       <imagenConDescripcion pieDeFoto="Primer plano del dispositivo que usa el rover Perseverance para mecanizar las rocas del suelo marciano, para su posterior estudio." :unLink = "'/vue-nds/assets/ZL0_0291_0692781524_700EBY_N0090000ZCAM05066_0340LMJ01_1200perseverance.jpg'" />
--- src/components/imagenConDescripcion.vue
+++ src/components/imagenConDescripcion.vue
@@ -3,10 +3,10 @@
 
     <div class="contenedorDeFoto">
 <img :src=unLink alt="pieDeFoto" > 
-<p class="textoPieDeFoto">{{ pieDeFoto }}</p>   
+<p class="textoPieDeFoto">{{ pieDeFoto }} </p>   
 </div>
 
-<img src="../assets/fotos/EBF_0001_0667022756_679ECV_N0010052EDLC00001_0010LUJ01_1200.jpg" alt="foto de prueba">
+<img src='../assets/fotos/EBF_0001_0667022756_679ECV_N0010052EDLC00001_0010LUJ01_1200.jpg' alt="foto de prueba">
 </template>
 
 <script>
